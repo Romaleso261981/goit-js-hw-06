f71c931b629a7e726fa9dd36ed5644e671fe1268
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-07-25 08:23:53 +0300

    зробив 1 і 2 задачку

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const list = document.querySelectorAll('li.item');
+
+console.log(`Number of categories:${list.length}`);
+
+// const ul = document.querySelectorAll('#categories>li');
+// ul.forEach(el => {
+
+//   const category = `Category: ${el.firstElementChild.textContent}`;
+//   const elements = `Elements: ${el.lastElementChild.children.length}`;
+
+//   console.log(category);
+//   console.log(elements);
+// });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulList = document.querySelector("#ingredients");
+
+
+
+const liItem = ingredients.map((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  return  listItem;
+});
+ulList.append(...liItem);
