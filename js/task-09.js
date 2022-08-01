2ec4fa8bd406c73413e260192b2dd71daa2455ba
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-08-01 10:55:28 +0300

    fix homework

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const bodyChangeColor = document.querySelector("body");
+const spanColorBody = document.querySelector(".color");
+
+
+changeColorBtn.addEventListener("click", changeColorBody);
+
+
+function changeColorBody() {
+  const color = getRandomHexColor();
+  bodyChangeColor.style.background = color;
+  spanColorBody.textContent = color;
+}
