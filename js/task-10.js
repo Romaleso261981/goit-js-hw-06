6e8c66ff80c7fe07e42d189f502a69f0102eaa12
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-08-04 14:04:43 +0300

    fix homework

--- js/task-10.js
+++ js/task-10.js
@@ -1,45 +1,44 @@
-
 const refs = {
-  valueToInput: document.querySelector('input'),
-  createBtn: document.querySelector('[data-create]'),
-  destroyBtn: document.querySelector('[data-destroy]'),
-  divsContainer: document.querySelector('#boxes'),
+  valueToInput: document.querySelector("input"),
+  createBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+  divsContainer: document.querySelector("#boxes"),
 };
+let currentNumber = 0;
 let startBoxSizing = 30;
- 
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function onCreateCounterInValueInput (value) {
-  refs.valueToInput.setAttribute('counter', value.currentTarget.value);
-};
+function onCreateCounterInValueInput(value) {
+  currentNumber = value.currentTarget.value;
+}
 
 let allDivsEl = [];
 function onCreateBoxes() {
- 
-  for (let i = 1; i <= refs.valueToInput.getAttribute('counter'); i += 1) {
+  for (let i = 1; i <= currentNumber; i += 1) {
     startBoxSizing += 10;
-    const createdDiv = document.createElement('div');
-    createdDiv.classList.add('box-item');
+    const createdDiv = document.createElement("div");
+    createdDiv.classList.add("box-item");
     createdDiv.style.width = `${startBoxSizing}px`;
     createdDiv.style.height = `${startBoxSizing}px`;
     createdDiv.style.backgroundColor = `${getRandomHexColor()}`;
-    createdDiv.style.marginBottom = '10px';
+    createdDiv.style.marginBottom = "10px";
     allDivsEl.push(createdDiv);
-    refs.divsContainer.append
+    refs.divsContainer.append;
   }
   refs.divsContainer.append(...allDivsEl);
-};
+}
 
-function onDestroyBoxes () {
-  refs.divsContainer.innerHTML = '';
-  // refs.valueToInput.removeAttribute('counter');
-  refs.valueToInput.value = '';
+function onDestroyBoxes() {
+  refs.divsContainer.innerHTML = "";
+  currentNumber = 0;
+  refs.valueToInput.value = "";
   allDivsEl = [];
   startBoxSizing = 30;
-};
+}
 
-refs.valueToInput.addEventListener('input', onCreateCounterInValueInput);
-refs.createBtn.addEventListener('click', onCreateBoxes);
-refs.destroyBtn.addEventListener('click',onDestroyBoxes);
+refs.valueToInput.addEventListener("input", onCreateCounterInValueInput);
+refs.createBtn.addEventListener("click", onCreateBoxes);
+refs.destroyBtn.addEventListener("click", onDestroyBoxes);
